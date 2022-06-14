25adfa6a44e22da924c6386f745283bcccbdda7f
Nick Piscopio <nick.piscopio@gmail.com>
2022-06-14 14:31:49 -0400

    Added Cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var pdfRouter = require('./src/routes/pdf');
+const pdfRouter = require('./src/routes/pdf');
 
-var app = express();
+const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
